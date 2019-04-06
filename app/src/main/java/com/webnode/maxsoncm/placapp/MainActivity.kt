b1b1e8621cd20cea8136e9147482e61f9908dac5
Maxson C. M <max_maidlinger@hotmail.com>
2019-04-06 15:54:21 -0300

    Initial commit

---
+++ app/src/main/java/com/webnode/maxsoncm/placapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.webnode.maxsoncm.placapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.webnode.maxsoncm.placapp.extensions.value
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btStart.setOnClickListener{
+            val nextScreenIntent = Intent(this, GameActivity::class.java)
+
+            nextScreenIntent.putExtra("HOME", inputHome.value())
+            nextScreenIntent.putExtra("AWAY", inputAway.value())
+
+            startActivity(nextScreenIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/webnode/maxsoncm/placapp/extensions/EditTextExt.kt
@@ -0,0 +1,5 @@
+package com.webnode.maxsoncm.placapp.extensions
+
+import android.widget.EditText
+
+fun EditText.value() = this.text.toString()
